66322e36b8c18dfcebf6908ee9120ee064905193
flipsome11 <155067757+flipsome11@users.noreply.github.com>
2023-12-29 18:51:56 +0800

    Add files via upload

---
+++ Passenger-Counter-App/index.js
@@ -0,0 +1,18 @@
+const saveEl = document.getElementById("save-el")
+const countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+// Google:
+// innerText alternative mdn
